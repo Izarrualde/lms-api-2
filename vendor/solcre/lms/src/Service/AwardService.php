7dcab5d3f2cbf15465be20c27f27f2ee58d8029f
Diego <diego.r.izarrualde@gmail.com>
2019-12-06 17:16:10 -0300

    remove(lms\services): in vendor

--- vendor/solcre/lms/src/Service/AwardService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Solcre\Lms\Service;
-
-use Solcre\SolcreFramework2\Service\BaseService;
-
-class AwardService extends BaseService
-{
-}
--- vendor/solcre/lms/src/Service/SessionService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Solcre\Lms\Service;
-
-use Solcre\SolcreFramework2\Service\BaseService;
-
-class SessionService extends BaseService
-{
-}
